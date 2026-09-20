dbbdabe659fc0d0fbba75a1edec11ce939ac9ae8
左智 <zuozhi@szlanyou.com>
2026-09-20 17:40:36 +0800

    chore: release MissionTab v0.2.31

--- MissionTab.spoon/init.lua
+++ MissionTab.spoon/init.lua
@@ -1,6 +1,6 @@
 --- === MissionTab ===
 --- Short Command-Tab switches applications; hold Command to navigate Mission Control.
-local obj = { name = 'MissionTab', version = '0.2.30', author = 'zuozhi', license = 'MIT' }
+local obj = { name = 'MissionTab', version = '0.2.31', author = 'zuozhi', license = 'MIT' }
 local directory = debug.getinfo(1, 'S').source:sub(2):match('(.*/)')
 local Session = dofile(directory .. 'session.lua')
 local MC = dofile(directory .. 'mission_control.lua')
